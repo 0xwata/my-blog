f7d439cbdafd5b2c1f332a2b84e64a7ec890f174
watarushi <watarutakamine@gmail.com>
2022-04-05 01:19:00 +0900

    add checklist page to header navi

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -3,6 +3,10 @@ const headerNavLinks = [
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
+  {
+    href: 'https://0xwata.notion.site/57b63aade5a245799877ef94e6b9c88d?v=d628db32357542f0879887aaafee4894',
+    title: 'CheckList',
+  },
 ]
 
 export default headerNavLinks
