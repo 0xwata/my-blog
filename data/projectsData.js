5f0a7d67d8089474b15fe1e5a1a6cd13e51f1322
0xwata <watarutakamine@gmail.com>
2023-03-04 02:04:40 +0900

    update project

--- data/projectsData.js
+++ data/projectsData.js
@@ -6,10 +6,22 @@ const projectsData = [
     href: 'https://www.sicf.jp/archives/archives20/winnerslist/',
   },
   {
-    title: 'Bunch: iOS App',
-    description: `メモアプリ(iOS)をReact Nativeで開発しました。`,
-    imgSrc: '/static/images/bunch.png',
-    href: 'https://www.sicf.jp/archives/archives20/winnerslist/',
+    title: 'AIpos',
+    description: 'IPO株購入抽選自動申込サービス',
+    imgSrc: '/static/images/Aipos.png',
+    href: 'https://prtimes.jp/main/html/rd/p/000000003.000064286.html',
+  },
+  {
+    title: 'Hometeeee',
+    description: `自己肯定感バグ上げサービス`,
+    imgSrc: '/static/images/hometeeee.png',
+    href: 'https://hometeeee.studio.site/',
+  },
+  {
+    title: 'Kaguya AI',
+    description: `AI占い師かぐやちゃん with chatGPT`,
+    imgSrc: '/static/images/kaguyaai.png',
+    href: 'https://github.com/0xwata/kaguya_ai',
   },
 ]
 
